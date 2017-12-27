7f0fd16d280c849190a0142eaed4231778c19af3
Noah Jacques <noah.jacques96@gmail.com>
2017-12-27 13:08:30 -0500

    sketched what should occur in the main method in comments

--- VMTranslator.java
+++ VMTranslator.java
@@ -19,6 +19,14 @@ public class VMTranslator{
 	*/
 	public static void main(String[] args){
 
+		//Determine if it's a directory or a .vm file
+		//If directory make list of all vm files
+		//If vm file then list is size one of that vm file
+		//Debatable whether or not to keep a list for single elements?
+
+		//Create parser for each vm file. One at a time or all at once? Multi-thread this if >1?
+		//Parse them all into command lists and write code for them all? Forget how this works.
+
 		CodeWriter cw = new CodeWriter();
 
 	}
